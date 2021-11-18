bf8dd29656df38ea601a55176f6755931122800a
Morgan Legare <83626394+MrNone1@users.noreply.github.com>
2021-11-18 19:06:28 +0000

    Initial commit

---
+++ dist/game.js
@@ -0,0 +1,20 @@
+(() => {
+  // code/main.js
+  add([
+    rect(1, 400),
+    pos(233, 100)
+  ]);
+  add([
+    rect(1, 400),
+    pos(366, 100)
+  ]);
+  add([
+    rect(400, 1),
+    pos(100, 233)
+  ]);
+  add([
+    rect(400, 1),
+    pos(100, 366)
+  ]);
+})();
+//# sourceMappingURL=game.js.map
